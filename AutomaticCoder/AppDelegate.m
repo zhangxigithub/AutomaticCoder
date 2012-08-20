7d985ce5411914714edeb76387d2beab80b904fd
zhangxi <zhangxi_1989@sina.com>
2012-08-20 15:54:55 +0800

    修正所有窗口都关闭后再也出不来的问题

--- AutomaticCoder/AppDelegate.m
+++ AutomaticCoder/AppDelegate.m
@@ -12,31 +12,17 @@
 
 @implementation AppDelegate
 
+-(BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
+{
+    if(flag == NO)
+    {
+        [self.window makeKeyAndOrderFront:nil];
+    }
+    return YES;
+}
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
-    
-    
-//    JsonObject *obj = [[JsonObject alloc] init];
-//    
-//    obj.name = @"张玺";
-//    obj.age = @5;
-//    obj.male = YES;
-//    obj.email = @"zzz";
-//    NSLog(@"%@",obj);
-//    
-//    NSDictionary *dic = @{ @"root" : obj };
-//    
-//    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-//
-//    NSData * myData = [NSKeyedArchiver archivedDataWithRootObject:dic];
-//    BOOL result = [myData writeToFile:[NSString stringWithFormat:@"%@/ddd",docDir] atomically:NO];
-//    
-//    NSData * ddd = [NSData dataWithContentsOfFile:[NSString stringWithFormat:@"%@/ddd",docDir]];
-//    NSDictionary * myDict = [NSKeyedUnarchiver unarchiveObjectWithData:ddd];
-//    JsonObject *bb = [myDict objectForKey:@"root"];
-//    NSLog(@"bb.name:%@",bb.name);
-    
+
 }
 
 - (IBAction)json:(id)sender {
