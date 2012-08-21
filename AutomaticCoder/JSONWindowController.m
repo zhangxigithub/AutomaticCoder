8add693b85459d26742171270a6e9fb6b27bc777
zhangxigithub <zhangxi_1989@sina.com>
2012-08-21 21:14:40 +0800

    增加对数组的支持，优化json显示

--- AutomaticCoder/JSONWindowController.m
+++ AutomaticCoder/JSONWindowController.m
@@ -65,7 +65,7 @@
             {
                 if([self isDataArray:[json objectForKey:key]])
                 {
-                    [proterty appendFormat:@"@property (nonatomic,strong) NSArray *%@%@;\n",preName.stringValue,key];
+                    [proterty appendFormat:@"@property (nonatomic,strong) NSMutableArray *%@%@;\n",preName.stringValue,key];
                     [import appendFormat:@"#import \"%@Entity.h\"",key];
                     [self generateClass:[NSString stringWithFormat:@"%@Entity",key] forDic:[[json objectForKey:key]objectAtIndex:0]];
                 }
@@ -132,7 +132,13 @@
             {
                 if([self isDataArray:[json objectForKey:key]])
                 {
-                    [config appendFormat:@"*********************************\n"];
+                    [config appendFormat:@"self.%@%@ = [NSMutableArray array];\n",preName.stringValue,key];
+                    [config appendFormat:@"for(NSDictionary *item in [json objectForKey:@\"%@\"])\n",key];
+                    [config appendString:@"{\n"];
+                    [config appendFormat:@"[self.%@%@ addObject:[[%@Entity alloc] initWithJson:item]];\n",preName.stringValue,key,key];
+                    [config appendString:@"}\n"];
+                    [encode appendFormat:@"[aCoder encodeObject:self.%@%@ forKey:@\"zx_%@\"];\n",preName.stringValue,key,key];
+                    [decode appendFormat:@"self.%@%@ = [aDecoder decodeObjectForKey:@\"zx_%@\"];\n ",preName.stringValue,key,key];
                 }
             }
                 break;
@@ -190,131 +196,26 @@
     NSData *data = [NSURLConnection sendSynchronousRequest:request
                                                         returningResponse:nil
                                                                     error:nil];
-    jsonContent.string = [[NSString  alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSDictionary *json = [data objectFromJSONData];
+    
+    jsonContent.string = [json JSONStringWithOptions:JKSerializeOptionPretty error:nil];
 }
 
 
 
 
 - (IBAction)generateClass:(id)sender {
-   // NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-   // NSString *name       = jsonName.stringValue;
     NSDictionary *json   = [jsonContent.string objectFromJSONString];
     
     if(json == nil)
     {
-        jsonContent.string = @"json格式不对吧。。。。。";
+        jsonContent.string = @"介个...json格式不对吧。。。。。";
         return;
     }
     
     [self generateClass:jsonName.stringValue forDic:json];
     
-    jsonContent.string = @"生成了.h.m(ARC)文件，给您放桌面上了，看看格式对不对。哦，对了，目前还不支持json里面嵌套数组和字典，以后可能会加上。"; 
-//    
-//    //准备模板
-//    NSMutableString *templateH =[[NSMutableString alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"json" ofType:@"zx1"]
-//                                                                       encoding:NSUTF8StringEncoding
-//                                                                          error:nil];
-//    NSMutableString *templateM =[[NSMutableString alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"json" ofType:@"zx2"]
-//                                                                       encoding:NSUTF8StringEncoding
-//                                                                          error:nil];
-//
-//
-//    //.h
-//    //name
-//    //property
-//    NSMutableString *proterty = [NSMutableString string];
-//   
-//    for(NSString *key in [json allKeys])
-//    {
-//        JsonValueType type = [self type:[json objectForKey:key]];
-//        switch (type) {
-//            case 0:
-//            case 1:
-//                [proterty appendFormat:@"@property (nonatomic,strong) %@ *%@%@;\n",[self typeName:type],preName.stringValue,key];
-//                break;
-//            case 2:
-//               [proterty appendFormat:@"@property (nonatomic,assign) %@ %@%@;\n",[self typeName:type],preName.stringValue,key];
-//                break;
-//            default:
-//                break;
-//        }
-//    }
-//
-//    [templateH replaceOccurrencesOfString:@"#name#"
-//                               withString:name
-//                                  options:NSCaseInsensitiveSearch
-//                                    range:NSMakeRange(0, templateH.length)];
-//    
-//    [templateH replaceOccurrencesOfString:@"#property#"
-//                               withString:proterty
-//                                  options:NSCaseInsensitiveSearch
-//                                    range:NSMakeRange(0, templateH.length)];
-//    
-//
-//
-//    
-//    //.m
-//    //NSCoding
-//    //name
-//    [templateM replaceOccurrencesOfString:@"#name#"
-//                               withString:name
-//                                  options:NSCaseInsensitiveSearch
-//                                    range:NSMakeRange(0, templateM.length)];
-//    
-//    
-//    NSMutableString *config = [NSMutableString string];
-//    NSMutableString *encode = [NSMutableString string];
-//    NSMutableString *decode = [NSMutableString string];
-//
-//    NSDictionary *list =  @{
-//    @"config":config,
-//    @"encode":encode,
-//    @"decode":decode
-//    };
-//    
-//    
-//    for(NSString *key in [json allKeys])
-//    {
-//        JsonValueType type = [self type:[json objectForKey:key]];
-//        switch (type) {
-//            case 0:
-//            case 1:
-//                [config appendFormat:@"self.%@%@  = [json objectForKey:@\"%@\"];\n ",preName.stringValue,key,key];
-//                [encode appendFormat:@"[aCoder encodeObject:self.%@%@ forKey:@\"zx_%@\"];\n",preName.stringValue,key,key];
-//                [decode appendFormat:@"self.%@%@ = [aDecoder decodeObjectForKey:@\"zx_%@\"];\n ",preName.stringValue,key,key];
-//                break;
-//            case 2:
-//                [config appendFormat:@"self.%@%@ = [[json objectForKey:@\"%@\"] booleanValue];\n ",preName.stringValue,key,key];
-//                [encode appendFormat:@"[aCoder encodeBool:self.%@%@ forKey:@\"zx_%@\"];\n",preName.stringValue,key,key];
-//                [decode appendFormat:@"self.%@%@ = [aDecoder decodeBoolForKey:@\"zx_%@\"];\n",preName.stringValue,key,key];
-//                break;
-//            default:
-//                break;
-//        }
-//    }
-//    
-//    //修改模板
-//    for(NSString *key in [list allKeys])
-//    {
-//        [templateM replaceOccurrencesOfString:[NSString stringWithFormat:@"#%@#",key]
-//                                   withString:[list objectForKey:key]
-//                                      options:NSCaseInsensitiveSearch
-//                                        range:NSMakeRange(0, templateM.length)];
-//    }
-//
-//    
-//    //写文件
-//    [templateH writeToFile:[NSString stringWithFormat:@"%@/%@.h",docDir,name]
-//                atomically:NO
-//                  encoding:NSUTF8StringEncoding
-//                     error:nil];
-//    [templateM writeToFile:[NSString stringWithFormat:@"%@/%@.m",docDir,name]
-//                atomically:NO
-//                  encoding:NSUTF8StringEncoding
-//                     error:nil];
-    
-    
+    jsonContent.string = @"生成了.h.m(ARC)文件，给您放桌面上了，看看格式对不对。";     
 }
 
 
